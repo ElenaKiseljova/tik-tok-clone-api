96aba597f3d6c2f4deeb90c8098918491ca53ea6
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-07-21 18:21:44 +0300

    7. Added: - CommentsModel - CommentsController - create_comment_table migration

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  //   public function index()
+  //   {
+  //     //
+  //   }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  //   public function create()
+  //   {
+  //     //
+  //   }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    $request->validate([
+      'post_id' => 'required',
+      'comment' => 'required',
+    ]);
+
+    try {
+      $comment = new Comment();
+
+      $comment->user_id = auth()->user()->id;
+      $comment->post_id = $request->validated('post_id');
+      $comment->text = $request->validated('text');
+
+      $comment->save();
+
+      return response()->json([
+        'success' => 'OK',
+      ], 200);
+    } catch (\Exception $e) {
+      return response()->json([
+        'error' => $e->getMessage(),
+      ], 400);
+    }
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  //   public function show(Comment $comment)
+  //   {
+  //     //
+  //   }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  //   public function edit(Comment $comment)
+  //   {
+  //     //
+  //   }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  //   public function update(Request $request, Comment $comment)
+  //   {
+  //     //
+  //   }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy($id)
+  {
+    try {
+      $comment = Comment::find($id);
+
+      $comment->delete();
+
+      return response()->json([
+        'success' => 'OK',
+      ], 200);
+    } catch (\Exception $e) {
+      return response()->json([
+        'error' => $e->getMessage(),
+      ], 400);
+    }
+  }
+}
